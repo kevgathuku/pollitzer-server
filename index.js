226bab9bcb1c8862b908bba66d7c1ab142fc06ee
Kevin Ndung'u <kevgathuku@gmail.com>
2016-12-06 21:10:41 +0300

    Implement setting initial state in the server app

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ import startServer from './src/server';
 // Create a store and export it
 export const store = makeStore();
 startServer(store);
+
+// Set our initial state
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
